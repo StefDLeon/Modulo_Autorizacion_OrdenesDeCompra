8caf59ce3d839a87b44c88aba6082afed951e5a7
StefDLeon <andrex.stef@gmail.com>
2017-11-07 17:44:35 -0600

    Repositorio Autorizacion Ordenes de compra

---
+++ Autorizacion_compras/Autorizacion_compras/Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace Autorizacion_compras
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbl_nBancoD = new System.Windows.Forms.Label();
+            this.lbl_tMonedaD = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.lbl_SaldoD = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lbl_nBancoO = new System.Windows.Forms.Label();
+            this.lbl_saldo = new System.Windows.Forms.Label();
+            this.lbl_Tmoneda = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_traslado = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(94, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(113, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(95, 18);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "No. de cuenta:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Banco:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 53);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Banco:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(11, 21);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "No. de cuenta:";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lbl_nBancoD);
+            this.groupBox1.Controls.Add(this.lbl_tMonedaD);
+            this.groupBox1.Controls.Add(this.label12);
+            this.groupBox1.Controls.Add(this.lbl_SaldoD);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Location = new System.Drawing.Point(314, 25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(264, 93);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cuenta Destino";
+            // 
+            // lbl_nBancoD
+            // 
+            this.lbl_nBancoD.AutoSize = true;
+            this.lbl_nBancoD.Location = new System.Drawing.Point(58, 52);
+            this.lbl_nBancoD.Name = "lbl_nBancoD";
+            this.lbl_nBancoD.Size = new System.Drawing.Size(0, 13);
+            this.lbl_nBancoD.TabIndex = 18;
+            // 
+            // lbl_tMonedaD
+            // 
+            this.lbl_tMonedaD.AutoSize = true;
+            this.lbl_tMonedaD.Location = new System.Drawing.Point(194, 77);
+            this.lbl_tMonedaD.Name = "lbl_tMonedaD";
+            this.lbl_tMonedaD.Size = new System.Drawing.Size(0, 13);
+            this.lbl_tMonedaD.TabIndex = 17;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(110, 77);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(90, 13);
+            this.label12.TabIndex = 16;
+            this.label12.Text = "Tipo de moneda: ";
+            // 
+            // lbl_SaldoD
+            // 
+            this.lbl_SaldoD.AutoSize = true;
+            this.lbl_SaldoD.Location = new System.Drawing.Point(48, 77);
+            this.lbl_SaldoD.Name = "lbl_SaldoD";
+            this.lbl_SaldoD.Size = new System.Drawing.Size(0, 13);
+            this.lbl_SaldoD.TabIndex = 15;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(11, 77);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(40, 13);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "Saldo: ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lbl_nBancoO);
+            this.groupBox2.Controls.Add(this.lbl_saldo);
+            this.groupBox2.Controls.Add(this.lbl_Tmoneda);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Location = new System.Drawing.Point(26, 25);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(268, 92);
+            this.groupBox2.TabIndex = 15;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Cuenta Origen";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // lbl_nBancoO
+            // 
+            this.lbl_nBancoO.AutoSize = true;
+            this.lbl_nBancoO.Location = new System.Drawing.Point(58, 47);
+            this.lbl_nBancoO.Name = "lbl_nBancoO";
+            this.lbl_nBancoO.Size = new System.Drawing.Size(0, 13);
+            this.lbl_nBancoO.TabIndex = 24;
+            // 
+            // lbl_saldo
+            // 
+            this.lbl_saldo.AutoSize = true;
+            this.lbl_saldo.Location = new System.Drawing.Point(57, 76);
+            this.lbl_saldo.Name = "lbl_saldo";
+            this.lbl_saldo.Size = new System.Drawing.Size(0, 13);
+            this.lbl_saldo.TabIndex = 23;
+            // 
+            // lbl_Tmoneda
+            // 
+            this.lbl_Tmoneda.AutoSize = true;
+            this.lbl_Tmoneda.Location = new System.Drawing.Point(207, 77);
+            this.lbl_Tmoneda.Name = "lbl_Tmoneda";
+            this.lbl_Tmoneda.Size = new System.Drawing.Size(0, 13);
+            this.lbl_Tmoneda.TabIndex = 22;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(117, 76);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(90, 13);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "Tipo de moneda: ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(11, 76);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 13);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Saldo: ";
+            // 
+            // txt_traslado
+            // 
+            this.txt_traslado.Location = new System.Drawing.Point(252, 138);
+            this.txt_traslado.Name = "txt_traslado";
+            this.txt_traslado.Size = new System.Drawing.Size(100, 20);
+            this.txt_traslado.TabIndex = 17;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(147, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Valor de la compra: ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(71, 138);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 13);
+            this.label9.TabIndex = 21;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(35, 136);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(79, 37);
+            this.button6.TabIndex = 22;
+            this.button6.Text = "Pre-visualizar saldos";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(372, 134);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 23;
+            this.button4.Text = "Compra";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(497, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Submodulo: 5400";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 205);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_traslado);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Autorizacion de compras";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txt_traslado;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lbl_Tmoneda;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lbl_saldo;
+        private System.Windows.Forms.Label lbl_tMonedaD;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label lbl_SaldoD;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lbl_nBancoD;
+        private System.Windows.Forms.Label lbl_nBancoO;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Autorizacion_compras/Autorizacion_compras/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Autorizacion_compras
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string no_form = "5004";
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            string noCuenta = textBox1.Text;
+            negocios.ObtenerSaldo(noCuenta);
+            MessageBox.Show("El saldo de la cuenta " + noCuenta + " es " + negocios.SCuenta);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string nCuenta = textBox1.Text;
+            negocios.ObtenertMoneda(nCuenta);
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+             
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+           // string nBancoO;
+            string noCuentaO = textBox1.Text;
+            //nBancoO = Convert.ToString(oBancoCB.SelectedItem.ToString());
+            //negocios.ObtenerID(nBancoO);
+
+            negocios.ObtenerSaldo(noCuentaO);
+            negocios.ObtenertMoneda(noCuentaO);
+            negocios.ObtenerIdBanco(noCuentaO);
+            negocios.ObtenerID(negocios.nomBanco);
+
+            lbl_saldo.Text = Convert.ToString(negocios.saldo);
+            lbl_Tmoneda.Text = negocios.tMoneda;
+            lbl_nBancoO.Text = negocios.bancoN;
+            string noCuentaD = textBox4.Text;
+            //string nBancoD;
+
+            //nBancoD = Convert.ToString(dBancoCB.SelectedItem.ToString());
+            //negocios.ObtenerID(nBancoD);
+            negocios.ObtenerSaldo(noCuentaD);
+            negocios.ObtenertMoneda(noCuentaD);
+            negocios.ObtenerIdBanco(noCuentaD);
+            negocios.ObtenerID(negocios.nomBanco);
+
+            lbl_SaldoD.Text = Convert.ToString(negocios.saldo);
+            lbl_tMonedaD.Text = negocios.tMoneda;
+            lbl_nBancoD.Text = negocios.bancoN;
+
+
+
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            //Datos de origen/////////////////////////////////////////////////
+           
+            string noCuentaO = textBox1.Text;
+            negocios.ObtenerSaldo(noCuentaO);
+            negocios.ObtenertMoneda(noCuentaO);
+            double saldoO = negocios.saldo;
+
+            //Datos del destino////////////////////////////////////////////////
+            string noCuentaD = textBox4.Text;
+            negocios.ObtenerSaldo(noCuentaD);
+            negocios.ObtenertMoneda(noCuentaD);
+            double saldoD = negocios.saldo;
+
+            //Monto del traslado///////////////////////////////////////
+            double tras = Convert.ToDouble(txt_traslado.Text);
+
+            // LLamada a negocios ////////////////////////////////////
+            negocios.TraspasoSaldo(noCuentaO,noCuentaD,saldoO,saldoD,tras);
+            //Carga de labels;
+
+            negocios.ObtenerSaldo(noCuentaO);
+            negocios.ObtenertMoneda(noCuentaO);
+
+            lbl_saldo.Text = Convert.ToString(negocios.saldo);
+            lbl_Tmoneda.Text = negocios.tMoneda;
+
+           // string noCuentaD = textBox4.Text;
+            //string nBancoD;
+
+            //nBancoD = Convert.ToString(dBancoCB.SelectedItem.ToString());
+            //negocios.ObtenerID(nBancoD);
+            negocios.ObtenerSaldo(noCuentaD);
+            negocios.ObtenertMoneda(noCuentaD);
+            lbl_SaldoD.Text = Convert.ToString(negocios.saldo);
+            lbl_tMonedaD.Text = negocios.tMoneda;
+        }
+
+        public void Habilitador_timer()
+        {
+            timer1.Interval = 50400000;
+            timer1.Enabled = true;
+            // Hook up timer's tick event handler.
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            datos.actualizacion_saldos();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            negocios.ObtenerValorM("Dolares"); //Prueba de funcion Obtener ValorM   AUN NO FUNCIONAL
+            MessageBox.Show("Valor: "+negocios.VMoneda ); //Muestra valor moneda AUN NO FUNCIONAL
+
+        }
+    }
+}
